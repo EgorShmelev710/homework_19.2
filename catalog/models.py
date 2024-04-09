a907cfeaec9c61ca7645f1e68956e062e8e29924
Egor Shmelev <shmelevyegor25@gmail.com>
2024-04-10 01:35:00 +0300

    filled models.py

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='категория')
+    description = models.TextField(verbose_name='описание')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+    image = models.ImageField(upload_to='products/', verbose_name='изображение', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.IntegerField(verbose_name='цена')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
